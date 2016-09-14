b926c9c07523215ef6226d3a5d1bb6431d677ee5
Ben Limmer <ben@benlimmer.com>
2016-09-14 12:54:08 -0600

    Add tests.

--- tests/acceptance/in-app-messages-test.js
+++ tests/acceptance/in-app-messages-test.js
@@ -10,8 +10,47 @@ test('modals with URI ClickActions handle transition correctly', function(assert
     let $el = findWithAssert('.ab-in-app-message', 'body');
     assert.equal($el.length, 1);
   });
-  click('.ab-message-text', 'body');
+  click('.ab-in-app-message', 'body');
   andThen(function() {
     assert.equal(currentURL(), '/in-app-messages/example-1');
+  });
+});
+
+test('modals with one button ClickActions handle transition correctly', function(assert) {
+  visit('/in-app-messages');
+  click('#trigger-modal-with-one-button');
+  andThen(function() {
+    let $el = findWithAssert('.ab-in-app-message', 'body');
+    assert.equal($el.length, 1);
+  });
+  click('.ab-message-button', 'body');
+  andThen(function() {
+    assert.equal(currentURL(), '/in-app-messages/example-1');
+  });
+});
+
+test('modals with two button ClickActions handle transition correctly - first button', function(assert) {
+  visit('/in-app-messages');
+  click('#trigger-modal-with-two-buttons');
+  andThen(function() {
+    let $el = findWithAssert('.ab-in-app-message', 'body');
+    assert.equal($el.length, 1);
+  });
+  click('.ab-message-button:nth-of-type(1)', 'body');
+  andThen(function() {
+    assert.equal(currentURL(), '/in-app-messages/example-1');
+  });
+});
+
+test('modals with two button ClickActions handle transition correctly - second button', function(assert) {
+  visit('/in-app-messages');
+  click('#trigger-modal-with-two-buttons');
+  andThen(function() {
+    let $el = findWithAssert('.ab-in-app-message', 'body');
+    assert.equal($el.length, 1);
+  });
+  click('.ab-message-button:nth-of-type(2)', 'body');
+  andThen(function() {
+    assert.equal(currentURL(), '/in-app-messages/example-2');
   });
 });
