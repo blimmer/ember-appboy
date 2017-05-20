aac069cc6d8fef489326289463707a17db89c38a
Ben Limmer <ben@benlimmer.com>
2017-05-20 16:24:20 -0600

    Use ember-cli-deploy for github pages deployment instead.

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -48,7 +48,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/ember-appboy/';
+    ENV.rootURL = '/ember-appboy';
 
   }
 
